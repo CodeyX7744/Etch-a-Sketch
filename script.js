936e733b8c0582479fd31e7a8e26c74da0053454
CodeyX7744 <codeyx7744@gmail.com>
2024-10-09 13:36:54 +0800

    Create customize grid from prompt.

---
+++ script.js
@@ -0,0 +1,50 @@
+
+let grid = document.querySelector(".grid");
+grid.addEventListener("click", function(){
+    let sides = prompt("Enter the number of sides.");
+    makeGrid(sides);
+});
+
+function makeGrid(sides=16) {
+  
+    let container = document.querySelector(".container");
+    let square = null;
+    let containerSize = 960;
+    let boxSize = containerSize/sides;
+
+    if (container.hasChildNodes()) {
+        removeGrid();
+    }
+
+    for (let i = 1; i <= sides; i++) {
+        for (let j = 1; j <= sides; j++) {
+            square = document.createElement("div");
+            square.classList.add("box");
+            square.classList.add(`box${i}${j}`);
+            square.style.width = boxSize+"px";
+            square.style.height = boxSize+"px";
+
+            container.appendChild(square);
+        }
+    }
+    mouseOver();
+}
+
+function removeGrid(){
+    let container1 = document.querySelector(".container");
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => container1.removeChild(box));
+}
+
+ 
+
+function mouseOver() {
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => box.addEventListener("mouseover", function() { box.classList.add("mouseover");}));
+}
+
+makeGrid();
+mouseOver();
+
+
+
